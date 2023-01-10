4013d9af96aee84b291f5c5e2328eaa4bbc922c3
RegisLewis <regisaslewis@gmail.com>
2023-01-10 00:48:45 -0800

    second button configuring

--- script.js
+++ script.js
@@ -2,19 +2,20 @@ const h2 = document.createElement("h2");
 h2.textContent = "(Oh, God, I hope I don't screw this up)"
 document.querySelector("#container").appendChild(h2);
 
-const photoButton = document.getElementById("photo-button");
+const testButton = document.getElementById("test-button");
+const newDiv = document.createElement("div");
+newDiv.setAttribute("id", "new-div");
+newDiv.innerHTML = "Please, <em>oh please</em>, just <strong>work.</strong>";
+const newButtonDiv = document.createElement("div");
+newButtonDiv.setAttribute("id", "new-button-div");
+const newButton = document.createElement("button");
+newButton.setAttribute("id", "new-button");
+newButton.textContent = "...phew";
+
 function addSection() {
-    const newDiv = document.createElement("div");
-    newDiv.setAttribute("id", "new-div");
-    newDiv.innerHTML = "Please, <em>oh please</em>, just <strong>work.</strong>";
-    const newButtonDiv = document.createElement("div");
-    newButtonDiv.setAttribute("id", "new-button-div");
-    const newButton = document.createAttribute("button");
-    newButton.setAttribute("id", "new-button");
-    newButton.textContent = "...phew";
     document.body.appendChild(newDiv);
-    document.body.appendChild(newButtonDiv)
+    document.body.appendChild(newButtonDiv);
     document.getElementById("new-button-div").appendChild(newButton);
-    document.getElementById("photo-button").remove();
+    document.getElementById("test-button").remove();
 }
-photoButton.addEventListener("click", addSection);
+testButton.addEventListener("click", addSection);
